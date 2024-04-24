ccf845b145d727f5eea640011c58ee0d2baa0f62
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-04-24 20:17:34 +0200

    se limpia el codigo

--- js/main.js
+++ js/main.js
@@ -1,28 +1,5 @@
 'use strict';
 
-// Entendiendo como funciona el for
-// const ejemplo = ['hola', 'chao'];
-// for (const value of ejemplo) {
-//   console.log("iteracion: " + value);
-// }
-
-
-// Obtener el elemento <ul> al que deseas agregar el <li>
-// const taskList = document.querySelector('.task-list');
-
-// Crear un nuevo elemento <li>
-// let nuevoElemento = document.createElement("li");
-
-// Agregar texto al nuevo elemento <li>
-// nuevoElemento.textContent = "hola mundo";
-
-// Agregar el nuevo elemento <li> a la lista <ul>
-// taskList.appendChild(nuevoElemento);
-
-
-
-
-
 const tasks = [
   { name: "Recoger setas en el campo", completed: true },
   { name: "Comprar pilas", completed: true },
@@ -35,7 +12,6 @@ const tasks = [
 
 // Obtener el elemento <ul> al que deseas agregar el <li>
 const taskList = document.querySelector('.task-list');
-
 
 
 const cargaDatos = () => {
@@ -69,35 +45,3 @@ const cargaDatos = () => {
 };
 
 cargaDatos();
-  // funcin que recoge el evento clieck
-  // const xx = { name: 'Recoger setas en el campo', completed: true }
-  const taskFilter = document.querySelector ('.js-text-task-filter');
-  //taskFilter() => { }
-
-
-//2--> TAREA 2- tachado
-
-
-
-
-
-
-
-
-
-
-/*PONER EN FUNCIONAMIENTO AÑADIR NUEVA TAREA
-
-//1-> creo const del botón Agregar.
-const btnAdd = document.querySelector('.js-btn-add');
-//1.1-> creo const del input
-const textTask = document.querySelector('.js-text-task-add')
-
-//2-> creo la función para añadir tareas
-function addTask() {
-  event.preventDefalut();
-  console.log('lsdfjdk');
-  const addText = taskInput.value.push();
-  if (addText !== ""); 
-  addTask.claslist.addeventlistener('click', handleclick);
-};*/
