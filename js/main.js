bbdcbe66078808cde29a36059062ea13ffa03c7d
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-04-25 18:36:46 +0200

    cambios

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const handleCheckboxClick = (event) => {
+  const checkbox = event.target;
+  console.log(`ID del checkbox: ${checkbox.id}, Valor: ${checkbox.checked}`);
+
+  // Se obtiene la posicion del li en el array de tareas, para luego tacharla
+  const arrayIdCheckbox = checkbox.id.split('_');
+  // ['check', '0']
+  const posicionLi = arrayIdCheckbox[1];
+  // Armo el id del li que voy a modificar
+  const idLi = `list_${posicionLi}`;
+  // Obtengo el li
+  const liATachar = document.querySelector(`#${idLi}`);
+
+  // Modifico el li de acuerdo si el checkbox esta marcado (true) o no (false)
+  if (checkbox.checked) {
+    liATachar.classList.add('tachado');
+  } else {
+    liATachar.classList.remove('tachado');
+  }
+};
+
 const tasks = [
   { name: "Recoger setas en el campo", completed: true },
   { name: "Comprar pilas", completed: true },
@@ -11,36 +32,40 @@ const tasks = [
 ];
 
 // Obtener el elemento <ul> al que deseas agregar el <li>
-const taskList = document.querySelector('.task-list');
+const ul = document.querySelector('.task-list');
 
 
 const cargaDatos = () => {
   // Itero la lista de tasks
-  for (const task of tasks) {
-    console.log("task: " + task.name);
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
+    // console.log("task: " + task.name);
 
     // Crea un nuevo elemento <li>
-    let list = document.createElement("li");
+    let li = document.createElement("li");
+    li.id = `list_${i}`;
 
     // Crea un nuevo elemento <input>
     let listInput = document.createElement("input");
     listInput.type = "checkbox";
-    list.appendChild(listInput);
+    listInput.id = `check_${i}`; // el id del checkbox se corresponde con el del li
+    listInput.addEventListener('click', handleCheckboxClick);
+    li.appendChild(listInput);
 
     // Crea un nuevo elemento <span> para el nombre de la tarea
     let taskText = document.createElement("span");
     taskText.innerHTML = task.name;
-    list.appendChild(taskText);
+    li.appendChild(taskText);
 
     if (task.completed === true) {
       // Tacha e texto en el <li>
-      list.classList.add("tachado");
+      li.classList.add("tachado");
       // Checkea el input
       listInput.checked = true;
     }
 
     // Agrega el nuevo elemento <li> a la lista <ul>
-    taskList.appendChild(list);
+    ul.appendChild(li);
   }
 };
 
